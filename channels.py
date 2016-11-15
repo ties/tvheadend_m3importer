e07682a6a55be8d33f1b563f2d6dbd1277dca75d
Ties de Kock <ties@tiesdekock.nl>
2016-11-15 13:05:17 +0100

    Update for 4.1-2286~gb15b02c and reliability
      * Update the script to work with the new API endpoints
        of Tvheadend 4.1-2286~gb15b02c
      * Do not re-add a channel that already exists
      * Make sure that the correct type of UUID is used. Previously the
        wrong type of object's uuid was used (a mux instead of network)
        without any problems. This was changed in tvheadend

--- channels.py
+++ channels.py
@@ -66,8 +66,9 @@ Wrapper for a Tvheadend instance
 
 
 class TvheadendAPI(object):
-    def __init__(self, root, user=None, pw=None):
+    def __init__(self, root, user=None, pw=None, interface='eth0'):
         self.root_url = root
+        self.interface = interface
         self.auth = (user, pw) if user else None
 
     def post(self, sub_url, data):
@@ -76,15 +77,21 @@ class TvheadendAPI(object):
         res = requests.post(url,  data=data, auth=self.auth)
         return res.json()
 
+    def get(self, sub_url, params):
+        url = urllib.parse.urljoin(self.root_url, sub_url)
+
+        res = requests.get(url,  params=params, auth=self.auth)
+        return res.json()
+
     """
-    Add IPTV channel to the first Tvheadend network
+    Add IPTV channel to the first Tvheadend mux
     with the first index. Make sure that max input streams
     attribute of the Network is set low enough (!)
 
     channel: Channel namedtuple for given channel
     """
-    def add_network(self, channel):
-        # Get the UUID of the iptv network
+    def add_mux(self, channel):
+        # Get the UUID of the iptv network by assuming it is the first
         uuid_request = self.post("/api/idnode/load", data={
             'class': "mpegts_network",
             'enum': 1,
@@ -94,7 +101,7 @@ class TvheadendAPI(object):
         if not uuid_request['entries']:
             print("""Make sure that there exists a network in tvheadend.
 
-            It should be op IPTV type and the number of maximum input
+            It should be of IPTV type and the number of maximum input
             streams should be low.
 
             Tvheadend will try to subscribe to all the channels that get
@@ -107,29 +114,49 @@ class TvheadendAPI(object):
         self.post("/api/mpegts/network/mux_create", data={
             'uuid': network_uuid,
             'conf': json.dumps({
-                "enabled": True,
-                "skipinitscan": True,
+                "enabled": 1,
+                "skipinitscan": 1,
                 "iptv_muxname": channel.name,
                 "iptv_url": channel.url,
-                "iptv_interface": "eth0",
+                "iptv_interface": self.interface,
                 "charset": "AUTO"
             })
         })
 
+    def list_muxes(self):
+        res = self.post("/api/mpegts/mux/grid", data={
+            'start': 0,
+            'limit': 999999999,
+            'sort': 'name',
+            'dir': 'ASC',
+        })
+
+        for mux in res['entries']:
+            yield Channel(mux['name'], mux['iptv_url'], mux)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Bulk-add channels to Tvheadend, from a M3U file')
     parser.add_argument('m3u_file', type=argparse.FileType('r'))
-    parser.add_argument('tvheadend_url', help="URL to tvheadend")
+    parser.add_argument('tvheadend_url',
+                        help='URL to tvheadend, e.g. http://192.168.1.2:9981')
     parser.add_argument('--user', default=None, help="username")
     parser.add_argument('--password', default=None, help="password")
+    parser.add_argument('--interface', default='eth0',
+                        help='interface name TVHeadend tunes on (e.g. eth0)')
 
     args = parser.parse_args()
 
     m3u_parser = ParseVLC(args.m3u_file)
-    tvh = TvheadendAPI(args.tvheadend_url, args.user, args.password)
+    tvh = TvheadendAPI(args.tvheadend_url, args.user, args.password,
+                       args.interface)
+
+    # Create a dict of the URL's of known channels:
+    known_channels = {m.url: m for m in tvh.list_muxes()}
 
     for channel in m3u_parser:
-        print("Added channel {} at {}".format(channel.name, channel.url))
-        tvh.add_network(channel)
+        if channel.url not in known_channels:
+            print('added: {} at {}'.format(channel.name, channel.url))
+            tvh.add_mux(channel)
+        else:
+            print('skipped: {} at {}'.format(channel.name, channel.url))
